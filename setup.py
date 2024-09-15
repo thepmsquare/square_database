0a056a121f6ca2f0d121ead418a3a9d4b115ed01
Parth Mangtani <impmmmm@gmail.com>
2024-09-16 00:30:09 +0530

    :bookmark: update to 1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "square_database"
 
 setup(
     name=package_name,
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     package_data={
         package_name: ["data/*", "pydantic_models/*"],
