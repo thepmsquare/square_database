c48bedcc72ca881d2963a723bcf23f87b3a7feee
Parth Mangtani <impmmmm@gmail.com>
2024-10-31 18:16:50 +0530

    :art: formatting changes

--- square_database/utils/CommonOperations.py
+++ square_database/utils/CommonOperations.py
@@ -1,8 +1,8 @@
 def snake_to_capital_camel(snake_str):
     try:
-        components = snake_str.split('_')
+        components = snake_str.split("_")
         # Capitalize the first letter of each component except the first one
-        camel_case = ''.join(x.title() for x in components)
+        camel_case = "".join(x.title() for x in components)
         return camel_case
     except Exception:
         raise
